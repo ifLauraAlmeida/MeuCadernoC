d8ff8193291f8c7e79931575fa4df3889f2b2bbc
ifLauraAlmeida <oddlauraink@hotmail.com>
2025-11-23 13:23:43 -0300

    Alterações nas observações do código do trabalho pra deixar ele mais descritivo e comentado

--- aulapraticaum.c
+++ aulapraticaum.c
@@ -13,22 +13,28 @@ int main() {
     printf("Digite outro numero: \n");
     scanf("%d", &n2); // armazena n2 
 
-    printf("Só mais um número, juro que é o último: \n");
+    printf("Só mais um número, juro que é o último:\n");
     scanf("%d", &n3); // armazena n3
 
+    printf("Os três número são: %d, %d e %d.\n", n1, n2, n3 );
+
     //---- Calcula a soma dos 3 números.
+    printf("CALCULANDO A SOMA DOS 3 NÚMEROS...\n");
     int soma = n1 + n2 + n3;
     printf("A soma dos três números é:  %d\n", soma);
 
     //----Calcula a subtração dos 3 números.
+    printf("CALCULANDO A SUBTRAÇÃO DOS 3 NÚMEROS...\n");
     int subtracao = n1 - n2 - n3;
     printf("A subtração dos três números é: %d\n", subtracao);
 
     //----Calcula a multiplicação dos 3 números.
+    printf("CALCULANDO A MULTIPLICAÇÃO DOS 3 NÚMEROS...\n");
     int multiplicacao = n1 * n2 * n3;
     printf("A multiplicação dos três números é: %d\n", multiplicacao);  
 
     //----Calcula a divisão dos 3 números.
+    printf("CALCULANDO A DIVISÃO DOS 3 NÚMEROS...\n");
     int divisao = n1 / n2 / n3;
     printf("A divisão dos três números é: %d\n", divisao);
 
@@ -53,7 +59,7 @@ int main() {
 
     if (n1_positivo && n2_par) {
     printf("%d é positivo e %d é par\n", n1, n2);
-}
+    }
 
 
     return 0;
